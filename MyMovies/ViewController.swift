1a041b4a6be9824165e450c74398ad972b0d97a5
Renan Luz <ren4n@live.com>
2020-08-10 07:28:22 -0300

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Renan Luz on 10/08/20.
+//  Copyright © 2020 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
