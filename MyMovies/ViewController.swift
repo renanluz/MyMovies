03dc36050f1f5780e16b463e1b63187846c425e4
Renan Luz <ren4n@live.com>
2020-08-11 06:35:18 -0300

    Update ViewController

--- MyMovies/ViewController.swift
+++ MyMovies/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var movie: Movie?
+  
+    @IBOutlet weak var ivMovie: UIImageView!
+    @IBOutlet weak var lbDuration: UILabel!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbCategories: UILabel!
+    @IBOutlet weak var tvSummary: UITextView!
+    @IBOutlet weak var lbRating: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+        
+        if let movie = movie {
+            ivMovie.image = UIImage(named: movie.image)
+            lbTitle.text = movie.title
+            lbCategories.text = movie.categories
+            lbRating.text = "⭐️ \(movie.rating)/10"
+            lbDuration.text = movie.duration
+            tvSummary.text = movie.summary
+        }
     }
-
 
 }
 
